2cf565965e5afea2b1e20a1b7632191d29d4e642
Jacob Alber <jaalber@microsoft.com>
2024-02-16 16:12:10 -0500

    fix: typo

--- vowpalwabbit/fb_parser/tests/prototype_label.cc
+++ vowpalwabbit/fb_parser/tests/prototype_label.cc
@@ -408,7 +408,7 @@ prototype_label_t continuous_label(std::vector<VW::cb_continuous::continuous_lab
   VW::polylabel actual_label;
   actual_label.cb_cont.reset_to_default();
 
-  actual_label.cb_cont..costs.reserve(costs.size());
+  actual_label.cb_cont.costs.reserve(costs.size());
   for (size_t i = 0; i < costs.size(); i++) { actual_label.cb_cont.costs.push_back(costs[i]); }
 
   return prototype_label_t{fb::Label_ContinuousLabel, actual_label, {}};
